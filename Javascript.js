d9dd57a50849a72828cb6f84a8674f111c0d6476
marckisnotonfire <marcoaurelio.delbue@gmail.com>
2023-12-02 20:44:54 +0100

    Update intro fino a svg che scrolla orizzontale
    Non scrolla ancora in orizzontale ma ho setuppato una bozza della parte prima del sito più un div larghissimo (che conterrà poi il nostro svg con il grafico) che dopo provo a far scrollare in orizzonale muovendosi però in verticale col mouse

---
+++ Javascript.js
@@ -0,0 +1,14 @@
+// Get the #VerticalScroll and #megaSVG elements
+const verticalScroll = document.getElementById('VerticalScroll');
+const megaSVG = document.getElementById('megaSVG');
+
+// Add a scroll event listener to the #VerticalScroll element
+verticalScroll.addEventListener('scroll', () => {
+    // Calculate the horizontal scroll position based on the vertical scroll position
+    const horizontalScrollPosition = (verticalScroll.scrollTop / (verticalScroll.scrollHeight - verticalScroll.clientHeight)) * (megaSVG.scrollWidth - megaSVG.clientWidth);
+
+    // Set the horizontal scroll position on the #megaSVG element
+    megaSVG.scrollLeft = horizontalScrollPosition;
+});
+
+  
