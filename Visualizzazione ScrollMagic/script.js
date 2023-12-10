d8ad92c818419c872058b7566b578b1dd607e3c8
Federium <94871525+Federium@users.noreply.github.com>
2023-12-11 00:29:15 +0100

    Avanzamento 11-12
    Signed-off-by: Federium <94871525+Federium@users.noreply.github.com>

--- Visualizzazione ScrollMagic/script.js
+++ Visualizzazione ScrollMagic/script.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger)
 
-let container = document.getElementById("container");
+let container = document.getElementById("sfondo");
 
 gsap.to(container, {
   x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
@@ -10,8 +10,24 @@ gsap.to(container, {
     scroller: '#scroller',
     invalidateOnRefresh: true,
     pin: true,
+    markers: true,
     scrub: 1,
     top: 'top top',
-    end: () => "+=" + container.offsetWidth
+    end: () => "+=9000"/*  + container.offsetHeight */ 
   }
 })
+
+
+
+const arrayRange = (start, stop, step) =>
+  Array.from(
+    { length: (stop - start) / step + 1 },
+    (value, index) => start + index * step
+  );
+
+let range = arrayRange(0, 142, 1);
+
+// Aggiungi uno spazio tra i numeri utilizzando il tag span con stile CSS
+let spacedRange = range.map(number => `<span class="number">${number}</span>`);
+
+document.getElementById("numbers_content").innerHTML = spacedRange.join("");
